e2ce509e0dd459aec3a2b2f6622f2921ea613c7b
Nguyễn Ngọc Khang 01/01/2007 <nguyenngockhang11207@gmail.com>
2026-08-21 09:00:02 +0700

    Task 1 - sua lan 2

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
 from app.db.database import Base, engine
 from app.models import event, event_task, user
 from app.routers.health import health_check_router
+from app.core.exceptions import ExceptionBase
 
 app = FastAPI(
     title= "Event Management"
@@ -10,6 +13,39 @@ app = FastAPI(
 Base.metadata.create_all(bind = engine)
 
 app.include_router(health_check_router)
+
+@app.exception_handler(ExceptionBase)
+async def custom_exception_handler(request : Request, exc : ExceptionBase):
+    return JSONResponse(
+        status_code= exc.status_code,
+        content= {
+            "status_code": exc.status_code,
+            "message": exc.message,
+            "error": exc.error
+        }
+    )
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content={
+            "status_code": 422,
+            "message": "Dữ liệu đầu vào không hợp lệ",
+            "error": exc.errors(),
+        },
+    )
+
+@app.exception_handler(Exception)
+async def global_exception_handler(request: Request, exc: Exception):
+    return JSONResponse(
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        content={
+            "status_code": 500,
+            "message": "Đã xảy ra lỗi hệ thống",
+            "error": str(exc),
+        },
+    )
 
 @app.get("/")
 def test_connect():
